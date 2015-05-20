e37f6c2fc4d7aef220a0204f49b11d57906f389a
kricker <kricker@ncsa-mbp-19.att.net>
2015-05-20 00:59:33 -0500

    even more tweaks.

--- drupal-base-start.sh
+++ drupal-base-start.sh
@@ -3,6 +3,30 @@
 # Run the parent start script if there is one.
 echo "entering the start script ...."
 
+# First, we'll define our default db connection vars
+mysqlip=localhost && drupaldbname=drupal && drupaluname=root && drupalpwd=password && drupaldbport=3306
+kbdbsettings=$(echo $KB_APP_SETTINGS | jq '.databases.default.default.database');
+if [ -v KB_APP_SETTINGS ] && [ "$kbdbsettings" != "null" ]; 
+	then 
+	# If kalabox is being used, then we'll need some tools to parse the database settings
+	apt-get install jq
+	mysqlip=$(echo $KB_APP_SETTINGS | jq '.databases.default.default.host') 
+	drupaluname=$(echo $KB_APP_SETTINGS | jq '.databases.default.default.username')
+	drupalpwd=$(echo $KB_APP_SETTINGS | jq '.databases.default.default.password')
+	drupaldbport=$(echo $KB_APP_SETTINGS | jq '.databases.default.default.port'); 
+	drupaldbname=$(echo $KB_APP_SETTINGS | jq '.databases.default.default.database');
+fi
+
+# If not using Kalabox, then we'll check for environment variables that may have been passed
+# or auto-set in the docker run command.
+if [ -v MYSQL_PORT_3306_TCP_ADDR ]; then mysqlip=$MYSQL_PORT_3306_TCP_ADDR; fi
+if [ -v DRUPAL_DB_USERNAME ]; then drupaluname=$DRUPAL_DB_USERNAME; fi
+if [ -v DRUPAL_DB_PASSWORD ]; 
+	then drupalpwd=$DRUPAL_DB_PASSWORD; 
+else 
+	if [ -v MYSQ_ROOT_PASSWORD ]; then drupalpwd=$MYSQL_ROOT_PASSWORD; fi 
+fi
+
 # Download Drupal if not already there
 indexfile=/srv/www/siterooot/index.php
 if [ -f "$indexfile" ];
@@ -12,7 +36,7 @@ if [ -f "$indexfile" ];
 		echo "Site not installed. Pulling latest drupal 7 ... "
 		cd /srv/www && drush dl drupal && mv /srv/www/drupal-7*/* /srv/www/siteroot
 		cd /srv/www/siteroot
-                rm index.html;
+        rm index.html && chown -R www-data:www-data /srv/www/siteroot;
 fi
 
 # Create files directory if it doesn't yet exist.
@@ -23,60 +47,42 @@ if [ ! -d "$filesdirectory" ]; then
 fi
 chmod a+w /srv/www/siteroot/sites/default -R
 
-if [ -v MYSQL_PORT_3306_TCP_ADDR ]; then mysqlip=$MYSQL_PORT_3306_TCP_ADDR else mysqlip=localhost; fi
-if [ -v DRUPAL_DB_USERNAME ]; then drupaluname=$DRUPAL_DB_USERNAME else drupaluname=root; fi
-if [ -v DRUPAL_DB_PASSWORD ]; 
-	then drupalpwd=$DRUPAL_DB_PASSWORD; 
-else 
-	if [ -v MYSQ_ROOT_PASSWORD ]; 
-		then drupalpwd=$MYSQL_ROOT_PASSWORD
-	else drupalpwd=password;
-	fi 
-fi
-
+# Configure settings.php 
 settingsfile=/srv/www/siterooot/sites/default/settings.php
-if [ ! -f "$settingsfile" ] && [ -v MYSQL_PORT_3306_TCP_ADDR ];
+
+if [ ! -f "$settingsfile" ];
+then
+	# Use drush to install a default generic drupal site and database installation
+    drush si -y minimal --db-url=mysql://${drupaluname}:${drupalpwd}@${mysqlip}/${drupaldbname} --account-pass=admin
+    chown -R www-data:www-data /srv/www/siteroot/sites;
+    installsite=true;
+fi
+
+# If we're not installing the site from scratch and we're using kalabox, then replace settings.php with kalabox settings.
+if [ ! -v installsite ] && [ "$kbdbsettings" != "null" ];
 	then
-	    drush si -y minimal --db-url=mysql://${drupaluname}:${drupalpwd}@${mysqlip}/drupal --account-pass=admin
-	    installsite=true;
-fi
+	# Alter the settings.php file to point to the right IP address.
+	sed -i "s/'host' => '.*'/'host' => '${mysqlip}'/g" /srv/www/siteroot/sites/default/settings.php
+	sed -i 's/"host" => ".*"/"host" => "${mysqlip}"/g' /srv/www/siteroot/sites/default/settings.php;
 
-# If the mysql environment variable exists, then alter the settings.php file to point to the right IP address.
-if [ -v MYSQL_PORT_3306_TCP_ADDR ]; 
-then 
-	sed -i "s/'host' => '.*'/'host' => '${MYSQL_PORT_3306_TCP_ADDR}'/g" /srv/www/siteroot/sites/default/settings.php
-	sed -i 's/"host" => ".*"/"host" => "${MYSQL_PORT_3306_TCP_ADDR}"/g' /srv/www/siteroot/sites/default/settings.php;
-fi
+	# Alter the settings.php file to configure the database name
+	sed -i "s/'username' => '.*'/'username' => '${drupaluname}'/g" /srv/www/siteroot/sites/default/settings.php
+	sed -i 's/"username" => ".*"/"username" => "${drupaluname}"/g' /srv/www/siteroot/sites/default/settings.php;
 
-# If the mysql environment variable exists, then alter the settings.php file to point to the right IP address.
-if [ -v DRUPAL_DB_USERNAME ]; 
-then 
-	sed -i "s/'username' => '.*'/'username' => '${DRUPAL_DB_USERNAME}'/g" /srv/www/siteroot/sites/default/settings.php
-	sed -i 's/"username" => ".*"/"username" => "${DRUPAL_DB_USERNAME}"/g' /srv/www/siteroot/sites/default/settings.php;
-fi
-
-if [ "$drupalpwd" != "password" ]; 
-then 
 	sed -i "s/'password' => '.*'/'password' => '${drupalpwd}'/g" /srv/www/siteroot/sites/default/settings.php
 	sed -i 's/"password" => ".*"/"password" => "${drupalpwd}"/g' /srv/www/siteroot/sites/default/settings.php;
-fi
 
-if [ -v DRUPAL_DB_NAME ]; 
-then 
-	sed -i "s/'database' => '.*'/'database' => '${DRUPAL_DB_NAME}'/g" /srv/www/siteroot/sites/default/settings.php
-	sed -i 's/"database" => ".*"/"database" => "${DRUPAL_DB_NAME}"/g' /srv/www/siteroot/sites/default/settings.php;
-fi
+	sed -i "s/'database' => '.*'/'database' => '${drupaluname}'/g" /srv/www/siteroot/sites/default/settings.php
+	sed -i 's/"database" => ".*"/"database" => "${drupaluname}"/g' /srv/www/siteroot/sites/default/settings.php;
 
-if [ -v DRUPAL_DB_PORT ]; 
-then 
-	sed -i "s/'port' => '.*'/'port' => '${DRUPAL_DB_PORT}'/g" /srv/www/siteroot/sites/default/settings.php
-	sed -i 's/"port" => ".*"/"port" => "${DRUPAL_DB_PORT}"/g' /srv/www/siteroot/sites/default/settings.php;
+	sed -i "s/'port' => '.*'/'port' => '${drupaldbport}'/g" /srv/www/siteroot/sites/default/settings.php
+	sed -i 's/"port" => ".*"/"port" => "${drupaldbport}"/g' /srv/www/siteroot/sites/default/settings.php;
 fi
 
 varnishdir=/srv/www/siteroot/sites/all/modules/varnish
 installmodules=false
 if [ -v installsite ] || [ -v REBUILD ]; then installmodules=true; fi
-if [ ! -v MYSQL_PORT_3306_TCP_ADDR ] || [ -d "$varnishdir" ]; then installmodules=false; fi
+if [ -d "$varnishdir" ]; then installmodules=false; fi
 
 if [ "$installmodules" = true ];
 	then
