f1aa734f21a5b98108f5e362e1ffd2a758248c1a
Keith Ricker <keithricker@hotmail.com>
2015-08-06 16:17:43 -0500

    Update drupal-base-start.sh

--- config/drupal-base-start.sh
+++ config/drupal-base-start.sh
@@ -34,7 +34,12 @@ fi
 if [ "${MYSQL_ADDRESS_EXT}" != "" ]; then dbsettings[host]="${MYSQL_ADDRESS_EXT}"; fi
 if [ "${MYSQL_DATABASE}" != "" ]; then dbsettings[database]="${MYSQL_DATABASE}"; fi
 
-if [ "${MYSQL_ENV_TUTUM_SERVICE_FQDN}" != "" ]; then dbsettings[host]="${MYSQL_ENV_TUTUM_SERVICE_FQDN}"; fi
+if [ "${MYSQL_ENV_TUTUM_SERVICE_FQDN}" != "" ]; 
+    then 
+        dbsettings[host]="${MYSQL_ENV_TUTUM_SERVICE_FQDN}"; 
+        MYSQL_PORT_3306_TCP_ADDR="${MYSQL_ENV_TUTUM_SERVICE_FQDN}"
+        MYSQL2_PORT_3306_TCP_ADDR="${MYSQL_ENV_TUTUM_SERVICE_FQDN}"
+fi
 
 if [ "${MYSQL_USERNAME}" != "" ]; then dbsettings[username]="${MYSQL_USERNAME}"; fi
 if [ "${DRUPAL_DB_USERNAME}" != "" ]; then dbsettings[username]="${DRUPAL_DB_USERNAME}"; fi
